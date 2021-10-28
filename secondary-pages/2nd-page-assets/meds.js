d9c746c2ccec6c7163767115261354d4ac35ec22
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-10-27 22:59:08 -0600

    delted all of daily schedule and put med card schedule instead

--- secondary-pages/2nd-page-assets/meds.js
+++ secondary-pages/2nd-page-assets/meds.js
@@ -40,6 +40,7 @@ var militaryHours = [
   "2100",
   "2200",
   "2300",
+  "2400",
 ];
 
 // momentjs time code
@@ -50,51 +51,52 @@ var loadTimeBlock = function (i) {
     createTimeBlock(i);
   }
 };
-var createTimeBlock = function (i) {
-  var newLi = $("<li>").addClass("list-group-item");
-  var newRow = $("<div></div>").addClass("row");
-  var newTime = $("<div></div>")
-    .addClass(
-      "col-2 d-flex align-items-center border border-dark  rgb(170, 50, 220, .6); justify-content-center "
-    )
-    .text(hours[i]);
-  var newColInput = $("<div></div>").addClass(
-    "col-8 bg-white d-flex align-items-left justify-content-center "
-  );
-  var newInput = $(
-    "<input type='textArea' style='height: 100%; width:100%' ></input>"
-  ).attr("id", hours[i]);
-  var saveBtn = $("<button type='button'></button>")
-    .addClass("saveBtn btn-default")
-    .attr("data-id", hours[i])
-    .append('<i class="fas fa-save"></i>');
-  var btnCol = $("<div></div>").addClass(
-    "col-1 has-background-white   d-flex align-items-center border border-dark  rgb(170, 50, 220, .6); justify-content-center"
-  );
-  var task = localStorage.getItem(hours[i]);
-  if (task) {
-    newInput.val(task);
-  }
-  var hour = moment().format("H");
-  if (hour > militaryHours[i]) {
-    newInput.addClass("has-background-grey-lighter text-black");
-  }
-  if (hour == militaryHours[i]) {
-    newInput.addClass("has-background-danger-light  has-text-black");
-  }
-  if (hour < militaryHours[i]) {
-    newInput.addClass("has-background-primary-light  has-text-black");
-  }
-  btnCol.append(saveBtn);
-  newColInput.append(newInput);
-  // append to ul list on the page
-  newRow.append(newTime, newColInput, btnCol);
-  newLi.append(newRow);
-  $("#list").append(newLi);
-};
-loadTimeBlock();
-$(".saveBtn").click(function () {
-  var taskHour = $(this).attr("data-id");
-  var taskText = $("#" + taskHour).val();
-  localStorage.setItem(taskHour, taskText);
-});
+// var createTimeBlock = function (i) {
+//   var newLi = $("<li>").addClass("list-group-item");
+//   var newRow = $("<div></div>").addClass("row");
+//   var newTime = $("<div></div>")
+//     .addClass(
+//       "col-2 d-flex align-items-center border border-dark  rgb(170, 50, 220, .6); justify-content-center "
+//     )
+//     .text(hours[i]);
+//   var newColInput = $("<div></div>").addClass(
+//     "col-8 bg-white d-flex align-items-left justify-content-center "
+//   );
+//   var newInput = $(
+//     "<input type='textArea' style='height: 100%; width:100%' ></input>"
+//   ).attr("id", hours[i]);
+//   var saveBtn = $("<button type='button'></button>")
+//     .addClass("saveBtn btn-default")
+//     .attr("data-id", hours[i])
+//     .append('<i class="fas fa-save"></i>');
+//   var btnCol = $("<div></div>").addClass(
+//     "col-1 has-background-white   d-flex align-items-center border border-dark  rgb(170, 50, 220, .6); justify-content-center"
+//   );
+//   var task = localStorage.getItem(hours[i]);
+//   if (task) {
+//     newInput.val(task);
+//   }
+//   var hour = moment().format("H");
+//   if (hour > militaryHours[i]) {
+//     newInput.addClass("has-background-grey-lighter text-black");
+//   }
+//   if (hour == militaryHours[i]) {
+//     newInput.addClass("has-background-danger-light  has-text-black");
+//   }
+//   if (hour < militaryHours[i]) {
+//     newInput.addClass("has-background-primary-light  has-text-black");
+//   }
+//   btnCol.append(saveBtn);
+//   newColInput.append(newInput);
+//   // append to ul list on the page
+//   newRow.append(newTime, newColInput, btnCol);
+//   newLi.append(newRow);
+//   $("#list").append(newLi);
+// };
+// loadTimeBlock();
+// $(".saveBtn").click(function () {
+//   var taskHour = $(this).attr("data-id");
+//   var taskText = $("#" + taskHour).val();
+//   localStorage.setItem(taskHour, taskText);
+//   console.log(taskText);
+// });
