ae246ac73e904b0e7200b96b597b74ea3be6a8f3
Danika Pearson <89046934+karmadog72@users.noreply.github.com>
2021-10-29 13:14:09 -0600

    added API key for covid-19, currently not working

--- secondary-pages/2nd-page-assets/meds.js
+++ secondary-pages/2nd-page-assets/meds.js
@@ -1,5 +1,12 @@
 var today = moment().format("MMMM Do YYYY, h:mm:ss a");
 var displayDate = (document.getElementById("date").innerHTML = today);
+var searchBtn = document.getElementById("search-btn");
+var newConfirmed = document.getElementById("newConfirmed");
+var newRecovered = document.getElementById("newRecovered");
+var totalDeaths = document.getElementById("totalDeaths");
+var lat;
+var lon;
+var unitedStates = document.getElementById("unitedStates");
 // var hours = [
 //   "6:00 am",
 //   "7:00 am",
@@ -21,27 +28,6 @@ var displayDate = (document.getElementById("date").innerHTML = today);
 //   "11:00 pm",
 //   "12:00 am",
 // ];
-// var militaryHours = [
-//   "0600",
-//   "0700",
-//   "0800",
-//   "0900",
-//   "1000",
-//   "1100",
-//   "1200",
-//   "1300",
-//   "1400",
-//   "1500",
-//   "1600",
-//   "1700",
-//   "1800",
-//   "1900",
-//   "2000",
-//   "2100",
-//   "2200",
-//   "2300",
-//   "2400",
-// ];
 
 // momentjs time code
 moment().format("MMMM Do YYYY, h:mm:ss a");
@@ -51,52 +37,31 @@ var loadTimeBlock = function (i) {
     createTimeBlock(i);
   }
 };
-// var createTimeBlock = function (i) {
-//   var newLi = $("<li>").addClass("list-group-item");
-//   var newRow = $("<div></div>").addClass("row");
-//   var newTime = $("<div></div>")
-//     .addClass(
-//       "col-2 d-flex align-items-center border border-dark  rgb(170, 50, 220, .6); justify-content-center "
-//     )
-//     .text(hours[i]);
-//   var newColInput = $("<div></div>").addClass(
-//     "col-8 bg-white d-flex align-items-left justify-content-center "
-//   );
-//   var newInput = $(
-//     "<input type='textArea' style='height: 100%; width:100%' ></input>"
-//   ).attr("id", hours[i]);
-//   var saveBtn = $("<button type='button'></button>")
-//     .addClass("saveBtn btn-default")
-//     .attr("data-id", hours[i])
-//     .append('<i class="fas fa-save"></i>');
-//   var btnCol = $("<div></div>").addClass(
-//     "col-1 has-background-white   d-flex align-items-center border border-dark  rgb(170, 50, 220, .6); justify-content-center"
-//   );
-//   var task = localStorage.getItem(hours[i]);
-//   if (task) {
-//     newInput.val(task);
-//   }
-//   var hour = moment().format("H");
-//   if (hour > militaryHours[i]) {
-//     newInput.addClass("has-background-grey-lighter text-black");
-//   }
-//   if (hour == militaryHours[i]) {
-//     newInput.addClass("has-background-danger-light  has-text-black");
-//   }
-//   if (hour < militaryHours[i]) {
-//     newInput.addClass("has-background-primary-light  has-text-black");
-//   }
-//   btnCol.append(saveBtn);
-//   newColInput.append(newInput);
-//   // append to ul list on the page
-//   newRow.append(newTime, newColInput, btnCol);
-//   newLi.append(newRow);
-//   $("#list").append(newLi);
-// };
-// loadTimeBlock();
-// $(".saveBtn").click(function () {
-//   var taskHour = $(this).attr("data-id");
-//   var taskText = $("#" + taskHour).val();
-//   localStorage.setItem(taskHour, taskText);
-//   console.log(taskText);
-// });
+
+function getForescast(e) {
+  e.preventDefault();
+  var unitedStates = document.getElementById("unitedStates").value;
+  var apiUrl = `https://api.covid19api.com/total/${country}/${unitedStates}`;
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log("unitedStates is:", data.main.unitedStates);
+      console.log("confirmed is:", data.main.newConfirmed);
+      console.log("recovered is:", data.main.newRecovered);
+      console.log("Deaths is:", data.totalDeaths);
+
+      unitedStates.textContent = "Country-" + data.name;
+      confirmed.textContent = "Confirmed Cases" + data.main.newConfirmed;
+      recovered.textContent = "Recovered" + data.main.newRecovered;
+      deaths.textContent = "Deaths" + data.totalDeaths;
+      lat = data.coord.lat;
+      lon = data.coord.lon;
+      getForecast();
+    });
+}
+document.getElementById("unitedStates").value = "";
+
+searchBtn.addEventListener("click", getForescast);
